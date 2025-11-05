541c902a9a6fa7399cb959370a7a9a82af233131
Jdot-Tdot <117394245+Jdot-Tdot@users.noreply.github.com>
2025-11-05 12:37:40 -0600

    Automatic Redirect to FHU group

--- slang-shame/src/App.tsx
+++ slang-shame/src/App.tsx
@@ -1,5 +1,4 @@
-//import { useState } from 'react'
-import {Routes, Route} from 'react-router';
+import {Routes, Route, Navigate} from 'react-router';
 import GroupPage from './pages/GroupPage';
 import ReportPage from './pages/ReportPage';
 
@@ -7,6 +6,8 @@ import ReportPage from './pages/ReportPage';
 function App() {
   return (
     <Routes>
+      {/*Route path="/" redirects to "/group-default" */}
+      <Route path="/" element={<Navigate to="/FHU" replace />} />
       <Route path='/:group' element={<GroupPage />} />
       <Route path='/:group/report/:id' element={<ReportPage />} />
     </Routes>
