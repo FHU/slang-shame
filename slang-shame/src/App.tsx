a5e6e7528687cc7d87938804ee08c6a98f88f155
Jdot-Tdot <117394245+Jdot-Tdot@users.noreply.github.com>
2025-11-13 15:48:12 -0600

    First Dev Page Routes

--- slang-shame/src/App.tsx
+++ slang-shame/src/App.tsx
@@ -2,7 +2,13 @@ import {Routes, Route, Navigate} from 'react-router';
 import GroupPage from './pages/GroupPage';
 import ReportPage from './pages/ReportPage';
 import LeaderBoardPage from './pages/LeaderBoardPage'; 
-import TestPage from './pages/TestPage'
+import TestPage from './pages/TestPage';
+import TestSlangSearch from './pages/TestSlangSearch';
+import TestSuspectSearch from './pages/TestSuspectSearch';
+import DevGroupEdit from './pages/DevGroupEdit';
+import DevSuspectEdit from './pages/DevSuspectEdit';
+import DevSlangEdit from './pages/DevSlangEdit';
+import DevReports from './pages/DevReports';
 
 
 function App() {
@@ -10,9 +16,20 @@ function App() {
     <Routes>
       {/*Route path="/" redirects to "/group-default" */}
       <Route path="/" element={<Navigate to="/FHU" replace />} />
-      <Route path='/test' element={<TestPage />} />
+
+      {/* Dev Pages */}
+      <Route path="/dev" element={<Navigate to="/dev/test" replace />} />
+      <Route path='/dev/test' element={<TestPage />} />
+      <Route path='/dev/search/slang' element={<TestSlangSearch />}/>
+      <Route path='/dev/search/suspects' element={<TestSuspectSearch />}/>
+      <Route path='/dev/api/group' element={<DevGroupEdit />} />
+      <Route path='/dev/api/person' element={<DevSuspectEdit />} />
+      <Route path='/dev/api/slang' element={<DevSlangEdit />} />
+      <Route path='/dev/reports' element={<DevReports />} />
+
+      {/* Normal User Front End */}
       <Route path='/:group' element={<GroupPage />} />
-      <Route path="/:group/leaderboard" element={<LeaderBoardPage />} /> {/* ✅ New route */}
+      <Route path="/:group/leaderboard" element={<LeaderBoardPage />} />
       <Route path='/:group/report/:id' element={<ReportPage />} />
       
     </Routes>
