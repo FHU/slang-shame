691a1f6a39c208cda3f5e7e36e6c9fa1c4513be6
Jdot-Tdot <117394245+Jdot-Tdot@users.noreply.github.com>
2025-11-17 16:27:44 -0600

    Better separation of Routes

--- slang-shame/src/App.tsx
+++ slang-shame/src/App.tsx
@@ -2,13 +2,7 @@ import {Routes, Route, Navigate} from 'react-router';
 import GroupPage from './pages/GroupPage';
 import ReportPage from './pages/ReportPage';
 import LeaderBoardPage from './pages/LeaderBoardPage'; 
-import TestPage from './pages/TestPage';
-import TestSlangSearch from './pages/TestSlangSearch';
-import TestSuspectSearch from './pages/TestSuspectSearch';
-import DevGroupEdit from './pages/DevGroupEdit';
-import DevSuspectEdit from './pages/DevSuspectEdit';
-import DevSlangEdit from './pages/DevSlangEdit';
-import DevReports from './pages/DevReports';
+import DevRoutes from './routes/DevRoutes';
 
 
 function App() {
@@ -18,14 +12,7 @@ function App() {
       <Route path="/" element={<Navigate to="/FHU" replace />} />
 
       {/* Dev Pages */}
-      <Route path="/dev" element={<Navigate to="/dev/test" replace />} />
-      <Route path='/dev/test' element={<TestPage />} />
-      <Route path='/dev/search/slang' element={<TestSlangSearch />}/>
-      <Route path='/dev/search/suspects' element={<TestSuspectSearch />}/>
-      <Route path='/dev/api/group' element={<DevGroupEdit />} />
-      <Route path='/dev/api/person' element={<DevSuspectEdit />} />
-      <Route path='/dev/api/slang' element={<DevSlangEdit />} />
-      <Route path='/dev/reports' element={<DevReports />} />
+      <Route path="/dev/*" element={<DevRoutes />} />
 
       {/* Normal User Front End */}
       <Route path='/:group' element={<GroupPage />} />
