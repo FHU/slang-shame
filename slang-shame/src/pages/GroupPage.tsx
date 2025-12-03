3fd3752f2e54e8dc683d97f01c89634fb1f54c84
Jdot-Tdot <117394245+Jdot-Tdot@users.noreply.github.com>
2025-12-02 19:38:40 -0600

    Getting Suspects From Group - Group Page (#10)
    * Using ReportButton Component
    
    * Updating Types and Adding Better Error

--- slang-shame/src/pages/GroupPage.tsx
+++ slang-shame/src/pages/GroupPage.tsx
@@ -1,35 +1,64 @@
-//import React from 'react'
+import {useState, useEffect } from 'react';
+import { db } from '../database';
 import GroupTitle from '../components/GroupTitle';
 //import PersonSelect from '../components/PersonSelect';
-import GroupLeaderboard from '../components/GroupLeaderboard';
-
-
+//import GroupLeaderboard from '../components/GroupLeaderboard';
+import type { Suspects } from "../utils/types";
 import { Link, useParams } from 'react-router'; 
+import { Query } from "appwrite";
+import ReportButton from '../components/ReportButton';
 
 export const GroupPage = () => {
-    const params = useParams();
+    const { group: groupName } = useParams();
+    const [suspects, setSuspects] = useState<Suspects[]>([])
 
+    useEffect(() => {
+        const getSuspects = async () => {
+            try {
+                const {total, rows} = await db.groups.list([Query.equal("groupName", groupName || "")]);
+                console.log(total)
+                console.log(rows);
+
+                // Only fetch suspects if we found a group
+                if (rows.length > 0) {
+                    const suspectsResult = await db.suspects.list([Query.equal("groupID", rows[0].$id)]);
+                    console.log(suspectsResult.total)
+                    console.log(suspectsResult.rows);
+                    setSuspects(suspectsResult.rows);
+                }
+                else
+                {
+                  throw new Error("This Group does not exist")
+                }
+            }
+            catch(error){
+                console.log(error)
+            }
+        };
+
+        getSuspects();
+    }, [groupName])
 
   return (
     <>
     
   <div>
     <GroupTitle />
-    {params.group}
+    {groupName}
   </div>
 
   <div className="flex justify-center items-center mt-4">
-    <Link
-      to={`/${params.group}/report/123`}
-      className="text-4xl font-bold text-blue-600 text-center no-underline"
-  >
-      Report Someone
-    </Link>
+    {suspects.map((s) => (
+            <article className='border-4 border-black' key={s.$id}>
+              {s.avatarURL && <img src={s.avatarURL}></img>}
+              <ReportButton reportId={s.$id} firstName={s.firstName} lastName={s.lastName}/>
+            </article> ))}
+
 
   </div>
 
   <div className="flex justify-center items-center mt-4">
-        <Link to={`/${params.group}/leaderboard`}>
+        <Link to={`/${groupName}/leaderboard`}>
           <div className="text-4xl font-bold text-blue-600 text-center no-underline">
             Leaderboard
           </div>
