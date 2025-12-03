cabafd4d6f7b24431d625d95ff7fd78c8accff7e
Jdot-Tdot <117394245+Jdot-Tdot@users.noreply.github.com>
2025-12-02 22:41:22 -0600

    Slang API Call

--- slang-shame/src/components/SlangList.tsx
+++ slang-shame/src/components/SlangList.tsx
@@ -1,8 +1,42 @@
 import SlangSelect from './SlangSelect';
+import { useState, useEffect } from 'react';
+import { db } from '../database';
+import type { Slang } from '../utils/types';
 
-function SlangList() {
+type Props = {
+  selectedId: string | null;
+  onSelect: (id: string) => void;
+};
+
+function SlangList({selectedId, onSelect}: Props) {
+  const [slang, setSlang] = useState<Slang[]>([])
+
+    useEffect(() => {
+        const getSlang = async () => {
+            try {
+                const {total, rows} = await db.slang.list();
+                console.log(total)
+                console.log(rows);
+                setSlang(rows);
+                }
+            catch(error){
+                console.log(error)
+            }
+        };
+
+        getSlang();
+    }, [])
   return (
-    <SlangSelect/>
+<>
+   {slang.map((row) => (
+        <SlangSelect
+          key={row.$id}
+          word={row.word}
+          selected={row.$id === selectedId}
+          onClick={() => onSelect(row.$id)}
+        />
+      ))}
+</>
   )
 }
 
